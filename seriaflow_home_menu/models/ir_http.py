abef5e5684f4c6fae3cbf7a59b2b864cd837098a
Willy Hanafi <willyhanafi@example.com>
2026-02-21 16:27:45 +0700

    [FIX] seriaflow_home_menu: Use ir.http to intercept redirects instead of controller overrides

---
+++ seriaflow_home_menu/models/ir_http.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import werkzeug
+
+from odoo import models
+from odoo.http import request
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    @classmethod
+    def _pre_dispatch(cls, rule, args):
+        super()._pre_dispatch(rule, args)
+        
+        # Intercept root path BEFORE any other controller handles it (like website)
+        # We only do this if the user is logged in
+        if request.httprequest.path == '/':
+            if request.session.uid and request.env.user._is_internal():
+                werkzeug.exceptions.abort(request.redirect('/odoo/home'))
+
+    @classmethod
+    def _post_dispatch(cls, response):
+        response = super()._post_dispatch(response)
+        
+        # Intercept redirects to the default backend '/odoo' and override with our Home Menu
+        if hasattr(response, 'location') and response.location:
+            if request.session.uid and request.env.user._is_internal():
+                # Strictly check if it's redirecting to the root backend (with or without query parameters)
+                if response.location == '/odoo' or response.location.startswith('/odoo?'):
+                    response.location = response.location.replace('/odoo', '/odoo/home', 1)
+                    
+        return response
